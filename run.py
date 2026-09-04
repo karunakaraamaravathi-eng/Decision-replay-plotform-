a0bf3f312d163cd1c36b59ee74096ea9ee87a396
karunakara.A <karunakaraamaravathi@gmail.com>
2026-09-04 19:10:53 +0530

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from app.config import settings
-
-if __name__ == "__main__":
-    print(f"[*] Starting {settings.PROJECT_NAME}...")
-    print(f"[*] Access Web UI at: http://127.0.0.1:8000")
-    print(f"[*] Access Swagger API Docs at: http://127.0.0.1:8000/docs")
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
