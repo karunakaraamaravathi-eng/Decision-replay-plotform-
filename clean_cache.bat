58427cf4eb72ac7bbf8cc59bb8d8d4e792bd8bb0
karunakara.A <karunakaraamaravathi@gmail.com>
2026-09-04 19:08:53 +0530

    Delete clean_cache.bat

--- clean_cache.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo [*] Cleaning Python __pycache__ and cache artifacts...
-for /d /r . %%d in (__pycache__) do @if exist "%%d" rd /s /q "%%d"
-for /d /r . %%d in (.pytest_cache) do @if exist "%%d" rd /s /q "%%d"
-del /s /q *.pyc >nul 2>&1
-echo [OK] Python cache cleaned successfully!
